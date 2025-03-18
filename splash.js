f161e88bf768c2ad5727d8f8f68da182bdcc2df9
Ame <americafloresdelrio079@gamil.com>
2025-03-18 09:27:41 -0600

    https://github.com/AmeZero/Pagina/tree/master

---
+++ splash.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+    setTimeout(function () {
+        window.location.href = "index.html"; // Redirige a la página principal
+    }, 3000); // Espera 3 segundos antes de redirigir
+});
